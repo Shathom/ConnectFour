8dd9f1c25e21f2403e40906a6e9e9412de223974
hkim484 <hkim484@uic.edu>
2021-10-08 15:05:17 -0500

    divided the tests

--- src/test/java/ConnectFourTests.java
+++ src/test/java/ConnectFourTests.java
@@ -32,14 +32,18 @@ class ConnectFourTests {
 				assertEquals(1, b1.player, "Oops, number of rows for GameButton class is not correct!");	
 			}
 		}
-		for(int col = 0; col < 7; col++) {				
-			for(int row = 0; row < 6; row++) {
-				b2 = new GameButton(row, col, 2);
-				assertEquals(col, b2.column, "Oops, number of columns for GameButton class is not correct!");
-				assertEquals(row, b2.row, "Oops, number of rows for GameButton class is not correct!");	
-				assertEquals(2, b2.player, "Oops, number of rows for GameButton class is not correct!");	
-			}
-		}
 	}
 
+	@Test
+	void TestGameButtonConstructor2() {
+	for(int col = 0; col < 7; col++) {				
+		for(int row = 0; row < 6; row++) {
+			b2 = new GameButton(row, col, 2);
+			assertEquals(col, b2.column, "Oops, number of columns for GameButton class is not correct!");
+			assertEquals(row, b2.row, "Oops, number of rows for GameButton class is not correct!");	
+			assertEquals(2, b2.player, "Oops, number of rows for GameButton class is not correct!");	
+		}
+	}
+	}
+	
 }
