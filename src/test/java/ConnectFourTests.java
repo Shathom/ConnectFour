25952281f494ced9471f67e290c68188f5c4426b
hkim484 <77253136+hkim484@users.noreply.github.com>
2021-10-09 08:12:09 -0500

    No need for connectfour class
    It's already included in JavaFXTemplate.java. Thus we don't need connectfour class. I think the test also should be name differently.(Original: MyTest.java)

--- src/test/java/ConnectFourTests.java
+++ src/test/java/ConnectFourTests.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import javafx.scene.control.Button;
 
 
-class ConnectFourTests {
+class MyTests {
 	
 	GameButton b1;
 	GameButton b2;
@@ -22,28 +22,28 @@ class ConnectFourTests {
 		
 	}
 	
-	@Test
-	void TestGameButtonConstructor() {
-		for(int col = 0; col < 7; col++) {				
-			for(int row = 0; row < 6; row++) {
-				b1 = new GameButton(row, col, 1);
-				assertEquals(col, b1.column, "Oops, number of columns for GameButton class is not correct!");
-				assertEquals(row, b1.row, "Oops, number of rows for GameButton class is not correct!");	
-				assertEquals(1, b1.player, "Oops, number of rows for GameButton class is not correct!");	
-			}
-		}
-	}
-
-	@Test
-	void TestGameButtonConstructor2() {
-	for(int col = 0; col < 7; col++) {				
-		for(int row = 0; row < 6; row++) {
-			b2 = new GameButton(row, col, 2);
-			assertEquals(col, b2.column, "Oops, number of columns for GameButton class is not correct!");
-			assertEquals(row, b2.row, "Oops, number of rows for GameButton class is not correct!");	
-			assertEquals(2, b2.player, "Oops, number of rows for GameButton class is not correct!");	
-		}
-	}
-	}
-	
+//	@Test
+//	void TestGameButtonConstructor() {
+//		for(int col = 0; col < 7; col++) {				
+//			for(int row = 0; row < 6; row++) {
+//				b1 = new GameButton(row, col, 1);
+//				assertEquals(col, b1.column, "Oops, number of columns for GameButton class is not correct!");
+//				assertEquals(row, b1.row, "Oops, number of rows for GameButton class is not correct!");	
+//				assertEquals(1, b1.player, "Oops, number of rows for GameButton class is not correct!");	
+//			}
+//		}
+//	}
+//
+//	@Test
+//	void TestGameButtonConstructor2() {
+//	for(int col = 0; col < 7; col++) {				
+//		for(int row = 0; row < 6; row++) {
+//			b2 = new GameButton(row, col, 2);
+//			assertEquals(col, b2.column, "Oops, number of columns for GameButton class is not correct!");
+//			assertEquals(row, b2.row, "Oops, number of rows for GameButton class is not correct!");	
+//			assertEquals(2, b2.player, "Oops, number of rows for GameButton class is not correct!");	
+//		}
+//	}
+//	}
+//	
 }
