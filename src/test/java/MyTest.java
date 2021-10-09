9ff046dabbe73528e689e00811392ebb946b9bc4
hkim484 <77253136+hkim484@users.noreply.github.com>
2021-10-09 08:15:21 -0500

    Added myTest for GameLogic testing
    Added comment as well.

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,34 @@
+// * Project02 *//
+// MyTestt.java
+// ****************************************************************************/
+// University of Illinois at Chicago
+// Class: CS 342, FALL 2021
+// Author: Haeun Kim, UIC
+// Author: Sharon Thomeh
+// UIN number: 657655430
+// Connect Four JavaFX GUI
+// Implement the classic game of Connect Four. This is a simple game to
+// understand and play which should allow you to focus on learning GUI
+// development in JavaFx and trying your hand at event driven programming.
+// ****************************************************************************/
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// Testing GameLogic class 
+class MyTest {
+
+	@BeforeEach
+	void init() {
+		
+	}	
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
